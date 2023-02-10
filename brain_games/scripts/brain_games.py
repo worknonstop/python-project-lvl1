9f35424472f95f46bf85b1f5ff2bed46e4a90ef5
Yuriy Y <monkcrazy91@gmail.com>
2023-02-10 12:49:09 +0600

    add greeting in brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,12 @@
 #!usr/bin/env python3
 
+import prompt
+
 
 def main():
     print("Welcome to the Brain Games!")
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}!")
 
 
 if __name__ == "__main__":
