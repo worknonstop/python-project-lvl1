214100105af6aa54839f854ed6dec17eaaa72c74
Yuriy Y <monkcrazy91@gmail.com>
2023-02-10 16:48:18 +0600

    add .gitignore and update

---
+++ brain_games/cli.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python3
+
+import prompt
+
+
+def welcome_user():
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}!")
+
+
+if __name__ == "__main__":
+    welcome_user()
