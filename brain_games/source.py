b3d2acbab3a01326314b9028cbd75dd4c5b654fb
Yuriy Y <monkcrazy91@gmail.com>
2023-02-11 22:04:47 +0600

    fix source.py

--- brain_games/source.py
+++ brain_games/source.py
@@ -17,7 +17,6 @@ def is_even():
     welcome()
     name = user_name()
     print(f"Hello {name}")
-    print('Answer "yes" if the number is even, otherwise answer "no"')
     wrong_no = "'no' is wrong answer ;/(. Correct answer was 'yes"
     wrong_yes = "'yes' is wrong answer ;/(. Correct answer was 'no'"
     try_again = f"Let's try again, {name}"
@@ -39,17 +38,14 @@ def is_even():
         elif uneven_number and yes_answer:
             print(wrong_yes)
             break
-        elif even_number and yes_answer:
-            print("Correct!")
-            i += 1
-        elif uneven_number and no_answer:
+        elif (even_number and yes_answer) or (uneven_number and no_answer):
             print("Correct!")
             i += 1
         else:
             print(f"Let's try again, {name}")
             break
 
-    if i == 3:
-        print(f"Congratulations, {name}!")
-    else:
-        print(try_again)
+        if i == 3:
+            print(f"Congratulations, {name}!")
+        else:
+            print(try_again)
