0c3515a4dba13526937d56a628d3b15dcd5127fc
Yuriy Y <monkcrazy91@gmail.com>
2023-02-11 19:38:46 +0600

    add function is_even in source.py

--- brain_games/source.py
+++ brain_games/source.py
@@ -1,10 +1,57 @@
+import random
+
 import prompt
 
 
 def welcome():
-    print("Welcome to the Brain Games!\nMay I have your name? ")
+    print("Welcome to the Brain Games!")
+    print("May I have your name? ", end="")
 
 
 def user_name():
     name = prompt.string("")
     return name
+
+
+def is_even():
+    welcome()
+    name = user_name()
+    print(f"Hello {name}")
+    print('Answer "yes" if the number is even, otherwise answer "no"')
+    wrong_no = "'no' is wrong answer ;/(. Correct answer was 'yes"
+    wrong_yes = "'no' is wrong answer ;/(. Correct answer was 'yes"
+    try_again = f"Let's try again, {name}"
+
+    i = 0
+    while i < 3:
+        number = random.randint(1, 100)
+        print(f"Question: {number}")
+        user_answer = prompt.string("")
+        no_answer = user_answer == "no"
+        yes_answer = user_answer == "yes"
+        even_number = number % 2 == 0
+        uneven_number = number % 2 != 0
+        print(f"Answer: {user_answer}")
+
+        if even_number and no_answer:
+            print(wrong_no)
+            print(try_again)
+            i = 0
+        elif uneven_number and yes_answer:
+            print(wrong_yes)
+            print(try_again)
+            i = 0
+        elif even_number and yes_answer:
+            print("Correct!")
+            i += 1
+        elif uneven_number and no_answer:
+            print("Correct!")
+            i += 1
+        else:
+            print(f"Let's try again, {name}")
+            i = 0
+
+    print(f"Congratulations, {name}!")
+
+
+is_even()
