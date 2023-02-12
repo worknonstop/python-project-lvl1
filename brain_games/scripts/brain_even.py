8e31c1d9a932aaa159c965c0355ba00a5bfa7a11
Yuriy Y <monkcrazy91@gmail.com>
2023-02-12 17:41:26 +0600

    fix import in brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,10 @@
 #!usr/bin/env python3
 
-from brain_games import source
+from brain_games.games import even
 
 
 def main():
-    source.is_even()
+    even.is_even()
 
 
 if __name__ == "__main__":
