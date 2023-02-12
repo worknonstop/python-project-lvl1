997c0c6cafed5f69c18e3907af900e877bbbc09f
Yuriy Y <monkcrazy91@gmail.com>
2023-02-12 17:31:37 +0600

    Update logic.py

---
+++ brain_games/games/logic.py
@@ -0,0 +1,17 @@
+import random
+
+import prompt
+
+
+def welcome():
+    print("Welcome to the Brain Games!")
+    print("May I have your name? ", end="")
+
+
+def user_name():
+    name = prompt.string("")
+    return name
+
+
+def hello_name():
+    print(f"Hello {user_name()}")
