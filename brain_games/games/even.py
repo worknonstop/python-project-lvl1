ee55d9bacea36f52e09f8063c748e494c02d9da9
Yuriy Y <monkcrazy91@gmail.com>
2023-02-13 14:13:38 +0600

    Refactoring even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -5,36 +5,22 @@ def is_even():
     logic.welcome()
     name = logic.user_name()
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    wrong_no = "'no' is wrong answer ;/(. Correct answer was 'yes'"
-    wrong_yes = "'yes' is wrong answer ;/(. Correct answer was 'no'"
 
     i = 0
     while i < 3:
         number = logic.random_number()
         print(f"Question: {number}")
         user_answer = logic.user_answer()
-        no_answer = user_answer == "no"
-        yes_answer = user_answer == "yes"
-        even_number = number % 2 == 0
-        uneven_number = number % 2 != 0
-        print(f"Answer: {user_answer}")
 
-        if even_number and no_answer:
-            print(wrong_no)
-            print(f"Let's try again, {name}")
-            break
-        elif uneven_number and yes_answer:
-            print(wrong_yes)
-            print(f"Let's try again, {name}")
-            break
-        elif (even_number and yes_answer) or (uneven_number and no_answer):
+        if logic.check_number(number) == user_answer:
             print("Correct!")
             i += 1
         else:
+            print(
+                f"{user_answer} is wrong answer ;/(."
+                + f"Correct answer was {logic.check_number}"
+            )
             print(f"Let's try again, {name}")
             break
-
-        if i == 3:
-            print(f"Congratulations, {name}!")
-        else:
-            print(f"Let's try again, {name}")
+    if i == 3:
+        print(f"Congratulations, {name}")
