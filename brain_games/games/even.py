6624cbd7d826c3c7606315cf7a5f140b0da32575
Yuriy Y <monkcrazy91@gmail.com>
2023-02-13 14:19:01 +0600

    Fix even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -9,17 +9,16 @@ def is_even():
     i = 0
     while i < 3:
         number = logic.random_number()
+        check = logic.check_number(number)
         print(f"Question: {number}")
         user_answer = logic.user_answer()
 
-        if logic.check_number(number) == user_answer:
+        if check == user_answer:
             print("Correct!")
             i += 1
         else:
-            print(
-                f"{user_answer} is wrong answer ;/(."
-                + f"Correct answer was {logic.check_number()}"
-            )
+            print(f"{user_answer} is wrong answer ;/(." +
+                  f"Correct answer was {check}")
             print(f"Let's try again, {name}")
             break
     if i == 3:
