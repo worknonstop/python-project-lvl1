a681ff6eaa24f028b3823042aecb7a20fbe8a279
Yuriy Y <monkcrazy91@gmail.com>
2023-02-16 02:04:09 +0600

    Add is_prime_game() in prime.py

---
+++ brain_games/games/prime.py
@@ -0,0 +1,26 @@
+from brain_games import logic
+
+
+def is_prime_game():
+    logic.welcome()
+    name = logic.user_name()
+    print('Answer "yes" if given number is prime. Otherwise answer "no"')
+
+    i = 0
+    while i < 3:
+        question = random.randint(1, 20)
+        print(f"Question: {question}")
+        user_answer = logic.user_answer()
+        answer = logic.is_prime(question)
+
+        if user_answer == answer:
+            print("Correct!")
+            i += 1
+        else:
+            print(
+                f"'{user_answer}' is wrong answer ;/(. " f"Correct answer was '{check}'"
+            )
+            print(f"Let's try again, {name}")
+            break
+        if i == 3:
+            print(f"Congratulations, {name}")
