0f1e8ca05160a6cbae71e1eee8e5d5d7535c2d9b
Yuriy Y <monkcrazy91@gmail.com>
2023-02-16 02:22:29 +0600

    Refactoring calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -8,17 +8,17 @@ def calc_game():
 
     i = 0
     while i < 3:
-        operation = logic.random_question_calc()
-        print(f"Question: {operation}")
+        question = eval(logic.random_question_calc())
+        print(f"Question: {question}")
         user_answer = logic.user_answer()
         print(f"Answer: {user_answer}")
-        if user_answer == str(eval(operation)):
+        if user_answer == str(eval(question)):
             print("Correct!")
             i += 1
         else:
             print(
                 f"'{user_answer}' is wrong answer ;/(."
-                + f"Correct answer was '{eval(operation)}'"
+                + f"Correct answer was '{question}'"
             )
             print(f"Let's try again, {name}")
             break
