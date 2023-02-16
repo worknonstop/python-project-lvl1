0be3dd21e8282b56935408c7d367b95b0e445589
Yuriy Y <monkcrazy91@gmail.com>
2023-02-16 13:40:54 +0600

    Fix variable in calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -8,17 +8,17 @@ def calc_game():
 
     i = 0
     while i < 3:
-        question = eval(logic.random_question_calc())
+        question = logic.random_question_calc()
         print(f"Question: {question}")
         print("Your answer: ", end="")
         user_answer = logic.user_answer()
-        if user_answer == eval(str(question)):
+        if user_answer == str(eval(question)):
             print("Correct!")
             i += 1
         else:
             print(
                 f"'{user_answer}' is wrong answer ;/(."
-                + f"Correct answer was '{question}'"
+                + f"Correct answer was '{str(eval(question))}'"
             )
             print(f"Let's try again, {name}")
             break
