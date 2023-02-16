b5772d094b1e4326383bfcf9b8417f1638437d6a
Yuriy Y <monkcrazy91@gmail.com>
2023-02-16 14:44:10 +0600

    Refactoring gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -10,10 +10,11 @@ def gcd_game():
     while i < 3:
         num1 = logic.random_number()
         num2 = logic.random_number()
-        if num1 > num2:
-            result = logic.gcd(num1, num2)
-        else:
-            result = logic.gcd(num2, num1)
+        result = logic.is_more(num1, num2)
+        # if num1 > num2:
+        #     result = logic.gcd(num1, num2)
+        # else:
+        #     result = logic.gcd(num2, num1)
 
         print(f"Question: {num1} {num2}")
         print("Your answer: ", end="")
